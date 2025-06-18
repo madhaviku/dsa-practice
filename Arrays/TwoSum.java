3d75f5bd53de401a4d518d0a6e1ceb3b7d1b88f9
Madhavi Kulkarni <40390212+madhaviku@users.noreply.github.com>
2025-06-18 18:37:06 +0530

    Create TwoSum.java

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,25 @@
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Approach :  HashMap to store indices. (I could have used two pointer approach but since I need to return the index of the elements, I dont want to sort it)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
