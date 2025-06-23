162e5a88a38d735ac0f5ee3886e6d8391f00d115
Madhavi Kulkarni <40390212+madhaviku@users.noreply.github.com>
2025-06-23 17:05:33 +0530

    Create middleofLinkedList.java

---
+++ LinkedList/middleofLinkedList.java
@@ -0,0 +1,27 @@
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Below approach involves Floyd's algorithm using two pointer approach.
+  
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+
+Time Complexity : O(N)
